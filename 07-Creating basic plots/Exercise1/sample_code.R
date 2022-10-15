440cc2799ef48922cde9651c68d0b92ea9afa3d0
ccrisc <78849337+ccrisc@users.noreply.github.com>
2022-10-15 22:06:31 +0200

    Update sample_code.R

--- 07-Creating basic plots/Exercise1/sample_code.R
+++ 07-Creating basic plots/Exercise1/sample_code.R
@@ -1,1 +1,11 @@
+url <- url("https://raw.githubusercontent.com/ccrisc/Marketing-analytics/main/transactions.csv")
+#Read in data
+
+##
+
+# check class PurchAmount
+class(myData$PurchAmount)
+
+# if not numeric, change class
+myData[,PurchAmount := as.numeric(PurchAmount)]
 
