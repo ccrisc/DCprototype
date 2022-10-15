5e36994a57fccdaae7ae10d0e0db4d83cafa28b1
ccrisc <78849337+ccrisc@users.noreply.github.com>
2022-10-15 22:46:12 +0200

    Update sample_code.R

--- 08-Formatting plots/Exercise1/sample_code.R
+++ 08-Formatting plots/Exercise1/sample_code.R
@@ -1,1 +1,5 @@
-
+plot(x=myData[, PurchAmount], y=myData[, Cost], xlab="Purchase Amount", ylab="Costs", 
+     main="Costs by Purchase Amount", pch= 21, bg="lightblue",
+     cex=1.5, cex.lab=1.3, cex.main=1.5, cex.axis=1, font.main=2,
+     #####
+     )
