707fa65be2a7a94a409913c7132283f59e5b6a73
ccrisc <78849337+ccrisc@users.noreply.github.com>
2022-10-15 22:44:58 +0200

    Update ex_solution.R

--- 08-Formatting plots/Exercise1/ex_solution.R
+++ 08-Formatting plots/Exercise1/ex_solution.R
@@ -1,1 +1,5 @@
-
+# Example solution 
+plot(x=myData[, PurchAmount], y=myData[, Cost], xlab="Purchase Amount", ylab="Costs", 
+     main="Costs by Purchase Amount", pch= 21, bg="lightblue",
+     cex=1.5, cex.lab=1.3, cex.main=1.5, cex.axis=1, font.main=2,
+     xlim = c(0, 5000), ylim = c(0,2500))
