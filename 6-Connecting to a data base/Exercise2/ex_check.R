46280670f0e9cf1a7b1ba316e6b41e7256e83b37
ccrisc <78849337+ccrisc@users.noreply.github.com>
2022-10-15 19:18:15 +0200

    Add files via upload

--- 6-Connecting to a data base/Exercise2/ex_check.R
+++ 6-Connecting to a data base/Exercise2/ex_check.R
@@ -1,5 +1,5 @@
 ex() %>% check_function("dbListFields") %>% {
-  check_arg(., "con") %>% check_equal("con")
-  check_arg(., "name") %>% check_equal("demographics")
+  check_arg(., 1) %>% check_equal("con")
+  check_arg(., 2) %>% check_equal("demographics")
 }
 
