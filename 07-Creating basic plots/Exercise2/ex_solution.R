f656701225b41b97432f19baa61d24d56f7f725f
ccrisc <78849337+ccrisc@users.noreply.github.com>
2022-10-15 22:13:24 +0200

    Update ex_solution.R

--- 07-Creating basic plots/Exercise2/ex_solution.R
+++ 07-Creating basic plots/Exercise2/ex_solution.R
@@ -1,1 +1,6 @@
 
+# create histogram
+hist(myData$PurchAmount) # use either $ or the data.table way (dt[,columnname]) to select a column
+hist(myData[, PurchAmount]) 
+
+hist(myData$PurchAmount, breaks = 50) # use either $ or the data.table way (dt[,columnname]) to select a column
