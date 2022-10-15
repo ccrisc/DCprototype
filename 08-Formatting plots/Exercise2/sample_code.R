41ddfbc7a59ed00a4e723dcd09b76fb7c6115a13
ccrisc <78849337+ccrisc@users.noreply.github.com>
2022-10-15 22:47:59 +0200

    Update sample_code.R

--- 08-Formatting plots/Exercise2/sample_code.R
+++ 08-Formatting plots/Exercise2/sample_code.R
@@ -1,1 +1,7 @@
-
+# Example solution 
+plot(x=myData[, PurchAmount], y=myData[, Cost], xlab="Purchase Amount", ylab="Costs", 
+     main="Costs by Purchase Amount", 
+     ####
+     bg="lightblue",
+     #########
+     xlim = c(0, 5000), ylim = c(0,2500))
