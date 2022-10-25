0d8daf2bf00d02502b1d435be5b3f7a3ea0805ab
ccrisc <78849337+ccrisc@users.noreply.github.com>
2022-10-25 22:15:10 +0200

    Update ex_check.R

--- 05-Basic techniques for investigating data object/Exercise5/ex_check.R
+++ 05-Basic techniques for investigating data object/Exercise5/ex_check.R
@@ -2,4 +2,3 @@ ex() %>% check_function("fwrite") %>% {
   check_arg(., "x") %>% check_equal("myData")
   check_arg(., "file") %>% check_equal("transactions_students_backup.csv")
 }
-success_msg(&quot;Awesome! Great job!&quot;)
