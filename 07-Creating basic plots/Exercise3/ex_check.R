378cdfb151412ecc4c69247253502ea80034ffbf
ccrisc <78849337+ccrisc@users.noreply.github.com>
2022-11-13 10:56:39 +0100

    Update ex_check.R

--- 07-Creating basic plots/Exercise3/ex_check.R
+++ 07-Creating basic plots/Exercise3/ex_check.R
@@ -1,6 +1,6 @@
 ex() %>% check_function("plot") %>% {
-    check_arg(., "x") %>% check_equal("myData[, PurchAmount]")
-    check_arg(., "y") %>% check_equal("myData[, Cost]")
+    check_arg(., "x") %>% check_equal()
+    check_arg(., "y") %>% check_equal()
   }
 ex() %>% check_function("cor")%>% {
     check_arg(., "x") %>% check_equal()
