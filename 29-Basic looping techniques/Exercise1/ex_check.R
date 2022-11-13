0256c5162334f046f1cfca8a239c17168fc8031c
ccrisc <78849337+ccrisc@users.noreply.github.com>
2022-11-13 12:11:51 +0100

    Update ex_check.R

--- 29-Basic looping techniques/Exercise1/ex_check.R
+++ 29-Basic looping techniques/Exercise1/ex_check.R
@@ -1,4 +1,4 @@
 ex() %>% check_for() %>% {
-  check_cond(.) %>% check_code("in\\s*1:10", missing_msg = "You can use `i in 1:10` to define your for loop.")
+  check_cond(.) %>% check_code("in\\s*10:1")
   check_body(.) %>% check_function("print") %>% check_arg("x") %>% check_equal(eval = FALSE)
 }
