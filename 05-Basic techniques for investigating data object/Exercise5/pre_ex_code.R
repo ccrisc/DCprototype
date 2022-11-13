7a299dfd7850706152101464dc7f6d5f8d6618a7
ccrisc <78849337+ccrisc@users.noreply.github.com>
2022-11-13 10:48:07 +0100

    Update pre_ex_code.R

--- 05-Basic techniques for investigating data object/Exercise5/pre_ex_code.R
+++ 05-Basic techniques for investigating data object/Exercise5/pre_ex_code.R
@@ -1,4 +1,4 @@
 library(lubridate)
 library(data.table)
-myData <- read.csv(url("https://raw.githubusercontent.com/ccrisc/Marketing-analytics/main/transactions.csv"), sep=',')
+myData <- fread("https://raw.githubusercontent.com/ccrisc/Marketing-analytics/main/transactions.csv")
 
