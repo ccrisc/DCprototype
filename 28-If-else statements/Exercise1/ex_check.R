c02d7381b596e014999b0ef421a32fc7e64373d4
ccrisc <78849337+ccrisc@users.noreply.github.com>
2022-11-13 11:58:27 +0100

    Update ex_check.R

--- 28-If-else statements/Exercise1/ex_check.R
+++ 28-If-else statements/Exercise1/ex_check.R
@@ -1,5 +1,5 @@
 ex() %>% check_object("q.type")
-ex() %>% check_if_else() %>% {
+check_if_else() %>% {
   check_cond(.) %>% check_code("q.type")
   check_if(.) %>% check_function("sample") %>% check_arg("x") %>% check_equal()
   check_else(.) %>% check_function("sample") %>% check_arg("x") %>% check_equal()
