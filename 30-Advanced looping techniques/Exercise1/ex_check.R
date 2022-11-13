b8db3f7e958fd552500c7fa4d50eeda49d308752
ccrisc <78849337+ccrisc@users.noreply.github.com>
2022-11-13 12:25:01 +0100

    Update ex_check.R

--- 30-Advanced looping techniques/Exercise1/ex_check.R
+++ 30-Advanced looping techniques/Exercise1/ex_check.R
@@ -1,7 +1,2 @@
 ex() %>% check_object("spending")
-ex() %>% check_function("system.time") %>% check_arg("expr") %>% check_equal(
-check_for() %>% {
-  check_cond(.) %>% check_code("in\\s*1:1000")
-  check_body(.) %>% check_object("spending") %>% check_equal(eval = FALSE)
-}
-)
+ex() %>% check_function("system.time") %>% check_arg("expr") %>% check_equal()
