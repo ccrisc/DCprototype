2fba4a1bec27856e747166e0e4cba6080eeef606
ccrisc <78849337+ccrisc@users.noreply.github.com>
2022-11-13 09:45:40 +0100

    Add files via upload

--- 21-Inner joins and full outer joins/Exercise2/ex_check.R
+++ 21-Inner joins and full outer joins/Exercise2/ex_check.R
@@ -1,6 +1,6 @@
 ex() %>% check_correct(
   check_object(., "e2") %>% check_equal(),
-  check_function('merge') %>% {
+  check_function(.,'merge') %>% {
     check_arg(., 1) %>% check_equal()
     check_arg(., 2) %>% check_equal()
     check_arg(., 'by') %>% check_equal()
