4ad65be8e393b8099aad35d90ec92ad46f30bf22
ccrisc <78849337+ccrisc@users.noreply.github.com>
2022-11-13 09:41:37 +0100

    Add files via upload

--- 07-Creating basic plots/Exercise3/ex_check.R
+++ 07-Creating basic plots/Exercise3/ex_check.R
@@ -1,4 +1,7 @@
-ex() %>% check_function("plot") %>% {
-  check_arg(., "x") %>% check_equal("myData[, PurchAmount]")
-  check_arg(., "y") %>% check_equal("myData[, Cost]")
+ex() %>% {
+  check_function("plot") %>% {
+    check_arg(., "x") %>% check_equal("myData[, PurchAmount]")
+    check_arg(., "y") %>% check_equal("myData[, Cost]")
+  }
+  check_function(., "cor")
 }
