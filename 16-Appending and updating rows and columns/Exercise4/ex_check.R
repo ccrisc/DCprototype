2f77be34f1f7b24e067540fc99ce1139ea44253d
ccrisc <78849337+ccrisc@users.noreply.github.com>
2022-11-13 12:49:52 +0100

    Update ex_check.R

--- 16-Appending and updating rows and columns/Exercise4/ex_check.R
+++ 16-Appending and updating rows and columns/Exercise4/ex_check.R
@@ -1,12 +1,10 @@
-ex() %>%  check_correct(
-  check_object(., "myData1") %>% check_equal(),
+ex() %>%  {
+  check_object(., "myData1") %>% check_equal()
   check_function(.,'copy') %>% {
     check_arg(., 'x') %>% check_equal('myData')
-  },
+  }
   check_output_expr(., 'myData1[, Profit := PurchAmount - Cost]')
-) & 
-  check_correct(
-    check_object(., "myData2") %>% check_equal(),
+    check_object(., "myData2") %>% check_equal()
     check_output_expr(., 'myData2[, Profit := PurchAmount - Cost]')
-  )
+}
 
